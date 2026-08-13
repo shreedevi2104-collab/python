a46334c5664974c7625a199eb88cbf8bc593944a
shreedevi2104-collab <shreedevi2104@gmail.com>
2026-08-13 09:09:53 +0530

    Create 3rd.py

---
+++ 3rd.py
@@ -0,0 +1,8 @@
+Check whether a number is even or odd using python programming language
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
