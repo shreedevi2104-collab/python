c33340ceb82e78390fb0ddaf5f6e9626ebb6af3c
shreedevi2104-collab <shreedevi2104@gmail.com>
2026-09-23 18:52:40 +0530

    Create 23th.py

---
+++ 23th.py
@@ -0,0 +1,10 @@
+# Check whether a number is positive, negative, or zero using the python programming language 
+
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
